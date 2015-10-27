fa74782088d1f40f3793c786e34dabf8212a64a3
TachyonRSA <TachyonRSA@users.noreply.github.com>
2015-10-27 20:12:57 +0200

    Added Project Files
    Added the current project files for the first release of the Epsilon VPN
    Client

---
+++ Project Files/VPN Client/Form1.Designer.vb
@@ -0,0 +1,326 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim ColumnHeader1 As System.Windows.Forms.ColumnHeader
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.chkRemember = New System.Windows.Forms.CheckBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtPSK = New System.Windows.Forms.TextBox()
+        Me.radL2TP = New System.Windows.Forms.RadioButton()
+        Me.radPPTP = New System.Windows.Forms.RadioButton()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtIP = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.lblVersion = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.RasPhoneBook1 = New DotRas.RasPhoneBook(Me.components)
+        Me.dialer = New DotRas.RasDialer(Me.components)
+        ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ColumnHeader1
+        '
+        ColumnHeader1.Text = "Date/Time"
+        ColumnHeader1.Width = 69
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(216, 358)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Connect"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 358)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Disconnect"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.chkRemember)
+        Me.GroupBox1.Controls.Add(Me.txtPassword)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.txtUsername)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 13)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(279, 100)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Account Information:"
+        '
+        'chkRemember
+        '
+        Me.chkRemember.AutoSize = True
+        Me.chkRemember.Location = New System.Drawing.Point(10, 77)
+        Me.chkRemember.Name = "chkRemember"
+        Me.chkRemember.Size = New System.Drawing.Size(95, 17)
+        Me.chkRemember.TabIndex = 4
+        Me.chkRemember.Text = "Remember Me"
+        Me.chkRemember.UseVisualStyleBackColor = True
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(71, 50)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(202, 20)
+        Me.txtPassword.TabIndex = 3
+        Me.txtPassword.UseSystemPasswordChar = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(7, 53)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(56, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Password:"
+        '
+        'txtUsername
+        '
+        Me.txtUsername.Location = New System.Drawing.Point(71, 17)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(202, 20)
+        Me.txtUsername.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(7, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Username:"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.txtPSK)
+        Me.GroupBox2.Controls.Add(Me.radL2TP)
+        Me.GroupBox2.Controls.Add(Me.radPPTP)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.txtIP)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Location = New System.Drawing.Point(13, 120)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(278, 87)
+        Me.GroupBox2.TabIndex = 4
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Server Information:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(9, 64)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(84, 13)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "Pre-Shared Key:"
+        '
+        'txtPSK
+        '
+        Me.txtPSK.Enabled = False
+        Me.txtPSK.Location = New System.Drawing.Point(99, 61)
+        Me.txtPSK.Name = "txtPSK"
+        Me.txtPSK.Size = New System.Drawing.Size(172, 20)
+        Me.txtPSK.TabIndex = 5
+        '
+        'radL2TP
+        '
+        Me.radL2TP.AutoSize = True
+        Me.radL2TP.Location = New System.Drawing.Point(117, 43)
+        Me.radL2TP.Name = "radL2TP"
+        Me.radL2TP.Size = New System.Drawing.Size(51, 17)
+        Me.radL2TP.TabIndex = 4
+        Me.radL2TP.Text = "L2TP"
+        Me.radL2TP.UseVisualStyleBackColor = True
+        '
+        'radPPTP
+        '
+        Me.radPPTP.AutoSize = True
+        Me.radPPTP.Checked = True
+        Me.radPPTP.Location = New System.Drawing.Point(64, 43)
+        Me.radPPTP.Name = "radPPTP"
+        Me.radPPTP.Size = New System.Drawing.Size(53, 17)
+        Me.radPPTP.TabIndex = 3
+        Me.radPPTP.TabStop = True
+        Me.radPPTP.Text = "PPTP"
+        Me.radPPTP.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(9, 45)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(49, 13)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "Protocol:"
+        '
+        'txtIP
+        '
+        Me.txtIP.Location = New System.Drawing.Point(117, 17)
+        Me.txtIP.Name = "txtIP"
+        Me.txtIP.Size = New System.Drawing.Size(154, 20)
+        Me.txtIP.TabIndex = 1
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(9, 20)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(102, 13)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "IP Address/Domain:"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.ListView1)
+        Me.GroupBox3.Location = New System.Drawing.Point(12, 214)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(279, 138)
+        Me.GroupBox3.TabIndex = 5
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "VPN Connection Log:"
+        '
+        'ListView1
+        '
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {ColumnHeader1, Me.ColumnHeader2})
+        Me.ListView1.Location = New System.Drawing.Point(6, 19)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(267, 113)
+        Me.ListView1.TabIndex = 0
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Information"
+        Me.ColumnHeader2.Width = 191
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1, Me.lblVersion})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 385)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(303, 22)
+        Me.StatusStrip1.SizingGrip = False
+        Me.StatusStrip1.TabIndex = 6
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(48, 17)
+        Me.ToolStripStatusLabel1.Text = "Version:"
+        '
+        'lblVersion
+        '
+        Me.lblVersion.Name = "lblVersion"
+        Me.lblVersion.Size = New System.Drawing.Size(40, 17)
+        Me.lblVersion.Text = "1.0.0.0"
+        '
+        'dialer
+        '
+        'TODO: Code generation for '' failed because of Exception 'Invalid Primitive Type: System.IntPtr. Consider using CodeObjectCreateExpression.'.
+        'TODO: Code generation for '' failed because of Exception 'Invalid Primitive Type: System.IntPtr. Consider using CodeObjectCreateExpression.'.
+        Me.dialer.Credentials = Nothing
+        Me.dialer.EapOptions = New DotRas.RasEapOptions(False, False, False)
+        Me.dialer.Options = New DotRas.RasDialOptions(False, False, False, False, False, False, False, False, False, False, False)
+        Me.dialer.SynchronizingObject = Me
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(303, 407)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Epsilon VPN Client"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtUsername As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents chkRemember As CheckBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtIP As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents radPPTP As RadioButton
+    Friend WithEvents radL2TP As RadioButton
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents ColumnHeader2 As ColumnHeader
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
+    Friend WithEvents lblVersion As ToolStripStatusLabel
+    Friend WithEvents txtPSK As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents RasPhoneBook1 As DotRas.RasPhoneBook
+    Friend WithEvents dialer As DotRas.RasDialer
+End Class
---
+++ Project Files/VPN Client/Form1.vb
@@ -0,0 +1,121 @@
+﻿'Creator: Tachyon
+'Date: 25 Ocotber 2015
+'Release Date: 27 October 2015
+'License: GNU GPL v2
+'If there are any issues please contact: tachyon@hackforums.org
+
+Imports DotRas
+Public Class Form1
+    Dim RasCon As RasConnection
+    Private Sub radL2TP_CheckedChanged(sender As Object, e As EventArgs) Handles radL2TP.CheckedChanged
+
+        'Basically, this sub just checks if the radio button for the L2TP has changed, if so, it must enable the Pre-Shared Key box to accept the PSK to
+        'complete the L2TP connection.
+
+        If radL2TP.Checked Then
+            txtPSK.Enabled = True 'Enables the TextBox if L2TP was selected
+        Else
+            txtPSK.Enabled = False 'Disables the TextBox when L2TP is unselected
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If My.Settings.Username.Equals("") Then 'Checks to see if there is a username stored in the settings
+        Else
+            txtUsername.Text = My.Settings.Username 'If there is, fill the inforamtion into the textbox 
+            chkRemember.Checked = True 'And check the checkbox to show that the remember function was enabled
+        End If
+    End Sub
+    Private Sub dialer_StateChanged(sender As Object, e As StateChangedEventArgs) Handles dialer.StateChanged
+        Dim li As ListViewItem 'Creates a new list view item to add items to the listview
+        li = ListView1.Items.Add(DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")) 'Adds the current date and time to the listview for the logs
+        li.SubItems.Add(e.State.ToString) 'Adds the current state of the connection to the listview for log purposes
+    End Sub
+
+    Private Sub chkRemember_CheckedChanged(sender As Object, e As EventArgs) Handles chkRemember.CheckedChanged
+        'This Sub basically checks if the checkbox for storing the users' username is checked, if it is store it, if not, dont
+        If chkRemember.Checked Then
+            My.Settings.Username = txtUsername.Text
+        Else
+            My.Settings.Username = ""
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim creds As Net.NetworkCredential 'Creates the new NetworkCredential required to authenticate to the VPN
+        Dim pptpEntry As RasEntry 'Creates an empty object of type RasEntry to be modified later for PPTP use
+        Dim l2tpEntry As RasEntry 'Creates an empty object of type RasEntry to be modified later for L2TP use
+
+        Try 'Start of Try Catch statement
+            RasPhoneBook1.Open(True) 'Opens the users' default phonebook to access VPN connection information
+
+            If txtUsername.Text = "" Then 'Makes sure that the user inputted a username
+                MessageBox.Show("Please enter a username.", "No username", MessageBoxButtons.OK, MessageBoxIcon.Error) 'If there was no username entered display an error message
+            Else 'If there is  username entered carry on
+                If txtPassword.Text = "" Then 'Makes sure that the user inputted a password 
+                    MessageBox.Show("Please enter a password.", "No password", MessageBoxButtons.OK, MessageBoxIcon.Error) 'If there was no password entered display an error
+                Else 'If there is a username and password entered carry on
+                    creds = New Net.NetworkCredential(txtUsername.Text, txtPassword.Text) 'Asigns the login information to the user credential object
+
+                    If radPPTP.Checked Then 'Checks to see which VPN protocol is required: PPTP
+
+                        For Each RasEntry In RasPhoneBook1.Entries.ToList 'Checks to see whether the VPN Connection already exists within the VPN Connection book
+                            If RasEntry.Name = txtIP.Text Then 'If it finds a entry do the following:
+                                dialVPN(txtIP.Text, RasPhoneBook.GetPhoneBookPath(RasPhoneBookType.User), creds) 'Dial the exisitng VPN with the user credentials the user provided
+                                Exit Sub 'Exits the Sub as the connection has been started
+                            End If 'End of the check of existing VPN Connections
+                        Next 'Moves on to next RasEntry in the users' VPN Phonebook
+
+                        pptpEntry = RasEntry.CreateVpnEntry(txtIP.Text, txtIP.Text, RasVpnStrategy.PptpOnly, RasDevice.GetDeviceByName("(PPTP)", RasDeviceType.Vpn, False))
+                        ' The above line creates a new VPN entry for the VPN connection as no VPN connections matching the criteria have been found
+                        RasPhoneBook1.Entries.Add(pptpEntry) 'Adds the VPN connection into the VPN phonebook
+
+                        dialVPN(txtIP.Text, RasPhoneBook.GetPhoneBookPath(RasPhoneBookType.User), creds) 'Starts the VPN connection to the newly created entry
+
+                    ElseIf radL2TP.Checked 'Checks to see which VPN protocol is required: L2TP/IPSEC
+
+                        For Each RasEntry In RasPhoneBook1.Entries.ToList 'Checks to see whether the VPN Connection already exists within the VPN Connection book
+                            If RasEntry.Name = txtIP.Text Then 'If it finds a entry do the following:
+                                dialVPN(txtIP.Text, RasPhoneBook.GetPhoneBookPath(RasPhoneBookType.User), creds) 'Dial the exisitng VPN with the user credentials the user provided
+                                Exit Sub 'Exits the Sub as the connection has been started
+                            End If 'End of the check of existing VPN Connections
+                        Next 'Moves on to next RasEntry in the users' VPN Phonebook
+
+                        l2tpEntry = RasEntry.CreateVpnEntry(txtIP.Text, txtIP.Text, DotRas.RasVpnStrategy.L2tpOnly, RasDevice.GetDeviceByName("(L2TP)", RasDeviceType.Vpn, False))
+                        ' The above line creates a new VPN entry for the VPN connection as no VPN connections matching the criteria have been found
+                        l2tpEntry.Options.UsePreSharedKey = True 'This tells the VPN Entry that this connection will require a Pre-Shared Key, obviously using the L2TP protocol
+
+                        RasPhoneBook1.Entries.Add(l2tpEntry) 'Adds this new VPN Connection into the phonebook
+
+                        l2tpEntry.UpdateCredentials(RasPreSharedKey.Client, txtPSK.Text) 'Updates the Pre-Shared key of the newly created VPN Connection
+
+                        dialVPN(txtIP.Text, RasPhoneBook.GetPhoneBookPath(RasPhoneBookType.User), creds) 'Starts the VPN connection to the newly created entry
+                    End If 'End of the If statement to see which protocol is required
+                End If 'End of the If statement to check the password field
+            End If 'Enf of the If statement to check the username field
+        Catch ex As Exception 'Catches any Exceptions
+            MessageBox.Show(ex.Message, Me.Text, MessageBoxButtons.OK, MessageBoxIcon.Error) 'Creates a MessageBox with the error that has occured
+        End Try
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim li As ListViewItem 'Creates a new listview item to add information to for use late on
+
+        RasCon = RasConnection.GetActiveConnectionByName(txtIP.Text, RasPhoneBook.GetPhoneBookPath(RasPhoneBookType.User)) 'Gets the current VPN connection
+        RasCon.HangUp() 'Ends the current VPN connection
+
+        ListView1.Items.Clear() 'Clears the listbox of all it's information
+
+        li = ListView1.Items.Add(DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")) 'Gets the current date/time and adds it to the listbox item
+        li.SubItems.Add("VPN Disconnected") 'Adds the information regarding what happened to the connection to the item and adds it to the listbox
+    End Sub
+
+    Public Sub dialVPN(ByVal entryName As String, ByVal bookPath As String, ByVal credentials As Net.NetworkCredential)
+        'Method to actually connect the the VPN that has been created/called
+        dialer.EntryName = entryName 'Sets the dialer to the name of the connection you are trying to use
+        dialer.PhoneBookPath = bookPath 'Sets the book path of where the connection information is actually stored
+        dialer.Credentials = credentials 'Sets the credentials required to login the the VPN service
+
+        dialer.DialAsync() 'Finally, it dials the VPN and connects to it (hopefully)
+    End Sub
+End Class
